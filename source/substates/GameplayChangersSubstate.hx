937398eba073b4314194e8fcdc596dbe8ece0352
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-04-12 15:10:33 +0800

    Update GameplayChangersSubstate.hx

--- source/substates/GameplayChangersSubstate.hx
+++ source/substates/GameplayChangersSubstate.hx
@@ -130,6 +130,9 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 
 		changeSelection();
 		reloadCheckboxes();
+
+		addTouchPad("LEFT_FULL", "A_B_C");
+		addTouchPadCamera();
 	}
 
 	var nextAccept:Int = 5;
@@ -260,7 +263,7 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 				}
 			}
 
-			if(controls.RESET)
+			if(controls.RESET || touchPad.buttonC.justPressed)
 			{
 				for (i in 0...optionsArray.length)
 				{
