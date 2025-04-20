05ac8927e54673df6f4f2c12bd6f1c77cfe0fd71
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-04-20 10:17:30 +0800

    Update MobileOptionsSubState.hx

--- source/mobile/options/MobileOptionsSubState.hx
+++ source/mobile/options/MobileOptionsSubState.hx
@@ -45,10 +45,6 @@ class MobileOptionsSubState extends BaseOptionsMenu
 		title = 'Mobile Options';
 		rpcTitle = 'Mobile Options Menu'; // for Discord Rich Presence, fuck it
 
-		option = new Option('Extra Controls', 'Select how many extra buttons you prefer to have?\nThey can be used for mechanics with LUA or HScript.',
-			'extraButtons', 'string', exControlTypes);
-		addOption(option);
-
 		option = new Option('Mobile Controls Opacity',
 			'Selects the opacity for the mobile buttons (careful not to put it at 0 and lose track of your buttons).', 'controlsAlpha', 'percent');
 		option.scrollSpeed = 1;
@@ -90,12 +86,6 @@ class MobileOptionsSubState extends BaseOptionsMenu
 			'If checked, the mobile controls color will be set to the notes color in your settings.\n(have effect during gameplay only)', 'dynamicColors',
 			'bool');
 		addOption(option);
-
-		#if android
-		option = new Option('Storage Type', 'Which folder Psych Engine should use?\n(CHANGING THIS MAKES DELETE YOUR OLD FOLDER!!)', 'storageType', 'string',
-			storageTypes);
-		addOption(option);
-		#end
 
 		super();
 	}
